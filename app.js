cb5426d23499f3c85185582ad919215ab4b0927c
João Vitor <romanijoaovitor@gmail.com>
2025-05-07 18:29:52 -0300

    adiciona função de inicialização do servidor

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const connectToDatabase = require('./config/db');
 const app = express();
+require('dotenv').config();
 
 // Middleware
 app.use(express.json());
+
+// Inicialização do servidor após conectar ao bd
+connectToDatabase()
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`));
+    });
