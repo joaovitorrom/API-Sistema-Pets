9e2a8f4528cc0f394a174e16d372612fe0800066
João Vitor <romanijoaovitor@gmail.com>
2025-05-08 15:38:54 -0300

    testa servidor e conexão com o MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const connectToDatabase = require('./config/db');
 const app = express();
-require('dotenv').config();
+const UserRoutes = require('./routes/UserRoutes');
 
 // Middleware
 app.use(express.json());
+
+// Routes
+app.use('/users', UserRoutes);
 
 // Inicialização do servidor após conectar ao bd
 connectToDatabase()
