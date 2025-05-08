2d8fb206a7bbb91333bebb0a7934408b444d6535
João Vitor <romanijoaovitor@gmail.com>
2025-05-07 23:32:51 -0300

    corrige erro de escrita

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-const moongose = require('moongose');
+const mongoose = require('mongoose');
 
 async function connectToDatabase(){
     try{
-        await moongose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Conectado ao MongoDB!");
     } 
     catch (err) {
